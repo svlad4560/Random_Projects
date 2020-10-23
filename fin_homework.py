9c8f78f4c297448cf62b9e983b1fc8653f67a960
svlad4560 <svlad4560@gmail.com>
2020-10-22 22:26:51 -0700

    This is a version that anwsers three questions. Window is outside first.

--- fin_homework.py
+++ fin_homework.py
@@ -1,10 +1,16 @@
 from tkinter import *
 
+# root = Tk()
+# root.title('Fin homework claculator')
+#
+# e = Entry(root, width=35, borderwidth=5)
+# e.grid(row = 0, column = 0, columnspan = 3, padx = 10, pady = 10)
 
 type_of_problem = str(input("""
 What are you trying to do?
 1: Find current stock price
 2: Find current dividend
+3: Find Payback Period
 Please type '1' or '2' and hit enter
 """))
 
@@ -24,6 +30,10 @@ if type_of_problem == "1":
     def find_purchase_price(grwoth_rate, rr, dividend):
         e.insert(0, dividend/(rr-grwoth_rate))
 
+    # def return_to_input():
+    #     e.delete(0, END)
+    #     input() we woudl need a loop in order to get back to the input screen
+
     requiered_stock_price = Button(root, text="Click for required purchase price", padx=40, pady=20, command=lambda: find_purchase_price(grwoth_rate, rr, dividend))
     requiered_stock_price.grid(row=1, column=1)
 
@@ -35,12 +45,6 @@ if type_of_problem == "2":
     grwoth_rate = .50 * rr
     current_stock_price = float(input('Give me the current stock price: '))
 
-    root = Tk()
-    root.title('Fin homework claculator')
-
-    e = Entry(root, width=35, borderwidth=5)
-    e.grid(row = 0, column = 0, columnspan = 3, padx = 10, pady = 10)
-
     def find_dividend(grwoth_rate, rr, current_stock_price):
         e.insert(0, (current_stock_price* grwoth_rate)/(1+grwoth_rate))
 
@@ -48,5 +52,23 @@ if type_of_problem == "2":
     requiered_dividend.grid(row=1, column=1)
 
     root.mainloop()
+
+if type_of_problem == "3":
+    print("Enter Cash flow for each year")
+    year0 = int(input("Year0: " ))
+    year1 = int(input("Year1: " ))
+    year2 = int(input("Year2: " ))
+    year3 = int(input("Year3: " ))
+    year4 = int(input("Year4: " ))
+
+    def find_payback_period(year0,year1,year2,year3, year4):
+        last_step = ((year0 - year1) - year2)- year3
+        e.insert(0,3+(last_step/year4))
+
+    paybackperiod = Button(root, text="click for payback period", padx=40, pady=20, command=lambda: find_payback_period(year0, year1, year2, year3, year4))
+    paybackperiod.grid(row=1, column=1)
+
+    root.mainloop()
+
 else:
     print('not valid choice')
